56d0acaa3941a9bac7fb26866458527a7c40c236
TFM-HITESH <hiteshshivkumar@gmail.com>
2025-04-04 15:10:08 +0000

    Fixed unescaped entities error

--- src/components/feedback-card.tsx
+++ src/components/feedback-card.tsx
@@ -13,7 +13,7 @@ export default function FeedbackCard({ name, branch, year, comment }: Props) {
     <div className="bg-sidebar-accent p-8 rounded-xl flex flex-col justify-center items-start gap-6 min-h-[18rem]">
       <ColourAvatar name={name} className="scale-125 ml-1" />
       <div className="flex text-wrap font-medium text-accent-foreground/80">
-        "{comment}"
+        &quot;{comment}&quot;
       </div>
       <div className="flex flex-col gap-1">
         <p className="text-lg font-semibold">{name}</p>
